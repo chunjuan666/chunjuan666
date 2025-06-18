742ce05c3ff5d9186e48b6b726a0599a25db0e45
chunjuan666 <3109968734@qq.com>
2025-06-18 16:16:44 +0800

    Delete daershixun directory

--- daershixun/System/src/MainCatering.java
+++
@@ -1,9 +0,0 @@
-import boundary.LoginFrame;
-
-public class MainCatering {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new LoginFrame().setVisible(true);
-        });
-    }
-}
